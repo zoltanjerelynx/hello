3803643de06de7fcb04b483f637a16f489f48de4
zjere <zoltan.jere@lynxsolutions.eu>
2015-12-07 13:01:29 +0200

    delete

--- hello.go
+++ hello.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+    "strconv"
+    "fmt"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+
+    // var x int = 404
+    // var y uint32 = 234242
+    // var xx float64 = 34.23
+    // var yy complex64 = 3 + 2i
+    // var weird string = "weird string"
+
+    // for i := 0; i < 10; i++ {
+	//        fmt.Println("The value of i : ", i)
+    // }
+
+    my_array := [ ]int{4, 3, 1, 7, 3, 9, 2}
+
+    converted, extra := convert_int_to_string_plus_extra(my_array)
+
+    fmt.Println("The value of sum : " + converted, extra)
+}
+
+
+func convert_int_to_string_plus_extra(a []int) (convert_to_string string, extra int) {
+    var sum int
+    for _, i := range a {
+        sum = sum + i
+    }
+    convert_to_string = strconv.Itoa(sum)
+    extra =  10
+
+    return convert_to_string, extra
 }
